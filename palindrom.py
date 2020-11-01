6d89e01b0b99428fee9c1ee69fa952d86ddb475c
rajkumar <rajkumarpandiyaraj@gmail.com>
2020-10-31 22:05:17 -0700

    palindrome using python

---
+++ palindrom.py
@@ -0,0 +1,6 @@
+string = input('Enter the word you want to check : ').lower()
+reversed_string = string[::-1]
+if reversed_string == string :
+    print('It is a palindrome')
+else :
+    print('i=It is not a palindrome')
